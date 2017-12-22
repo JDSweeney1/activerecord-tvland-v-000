37d216bb4cba849ec6154c59ad386c39ecf27dac
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-22 01:52:37 +0000

    automatically backed up by learn

--- db/migrate/009_add_catchphrase_to_character_Edit.rb
+++ db/migrate/009_add_catchphrase_to_character_Edit.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration
+class AddCatchphraseToCharacterEdit < ActiveRecord::Migration
   def change
     add_column :characters, :catchphrase, :string
     remove_column :characters, :catchprase
