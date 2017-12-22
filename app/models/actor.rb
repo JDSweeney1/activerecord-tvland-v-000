7fd0c381353339c0ecd48b90dd31c7a0d933f71e
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-22 01:18:39 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.collect{|s| s.first_name, s.last_name}
+    self.collect{|s| s.first_name && s.last_name}
   end
 end
